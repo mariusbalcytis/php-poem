37a3b066e6aa5e3264fd0c3b93cd86a4a2958edc
Marius Balčytis <marius.balcytis@gmail.com>
2017-04-19 20:08:59 +0300

    Declares opinion about who's the new boss in self-reciting way

---
+++ README.md
@@ -0,0 +1,16 @@
+# PHP is the new boss
+
+This is *self reciting poem* wrote as a submission for [Source Code Poetry](http://sourcecodepoetry.com/) competition.
+
+## Internals
+
+Poetry itself lives inside [`poetry.php`](poetry.php) file (duh).
+It has 1 companion file (conveniently named [`1`](1)) which helps to serve poem's purpose - to recite itself.
+
+## Demo
+
+![Poem recites itself](poetry.gif)
+
+## As an image
+
+![Code of the poem](poetry.png)
---
+++ poetry.php
@@ -0,0 +1,34 @@
+Hey! <?PHP $is_the = (new Boss())
+->when( @YOU); use It \ There\ Will\ Be\ No\ Chaos;
+@$_GET[ @TOTALLY_ANGRY or $raise (your)]; class
+Boss {VAR$IES = [his_features, so_we_could_keep_sitting_on_our_ass];
+
+protected $by = [most_of_fantastic_programmers,
+Boss_helps_them_to]; function when ($others){ $throw_hammers =
+$from = $you = $I = (require true) and sincere( $feelings);
+as_($this); global $decision; will( sure( make( some( $dealings ))));
+
+Warning.! $you_now_have_to_be || self:: $AWARE;
+hell:     $you^=ll_have_a ($difficult_choice_to); declare(
+ticks=0); are( TICKING and the( end( is_near())));
+for( $you; now(); $this-> path()) {is_truly_clear();
+
+list ($some) = [PROs and LOG_CONS, $fast];
+break; $this_loop; while (you^re . not( $last));}
+$hate ($${$this-> $language .${$or .LOVE. (it^s) . PERKS}});
+the( best( thing( is( that( ($this ->code)( works))))));
+
+switch ($your_side . IN){ case $you^re . $stuck:
+JAVA^s . $waiting_for_you_to . SUCK();
+continue; programming() - its_not_your_fault();
+$bad_things_happen; by(); default:}
+
+try { to_drink(); throw $up; if ($needed);
+} catch (A $cold) { if ($you .succeeded());
+$hope . you^ll;} finally { get_it_done();
+do {some( $stuff);} while (!having_fun());}
+
+if ($you^re . $really) {$ready-> to (DWELL);
+choose (PHP) . $or; goto hell;}
+return from_office_and();}} __halt_compiler();
+with PHP you won't need neither
